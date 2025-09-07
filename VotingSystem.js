2107357bdbb52d6f97ed635207c03afddc6862e7
Hunterly01 <mhunterly@gmail.com>
2025-09-07 13:14:29 +0100

    done

--- VotingSystem.js
+++ VotingSystem.js
@@ -2,6 +2,7 @@ const prompt = require("prompt-sync")();
 let candidates = [];
 let voters = [];
 let votes = {};
+let votedVoters = [];
 
 function registerCandidate(){
 	let name = prompt(" Enter candidate Name:");
@@ -33,12 +34,46 @@ function registerVoters(){
 }
 
 
+function toVote(){
+	let name = prompt(" Enter your Name:");
+	if (voters.includes(name) && !votedVoters.includes(name)) {
+		console.log("Candidates:");
+        candidates.forEach(candidate => console.log(candidate));
+        let choice = prompt("Enter the name of the candidate you want to vote for:");
+
+	if (candidates.includes(choice)) {
+            votes[choice]++;
+            votedVoters.push(name);
+            console.log("Vote cast successfully");
+        } else {
+            console.log("Invalid candidate");
+        }
+    } else if (votedVoters.includes(name)) {
+        console.log("You have already voted.");
+    } else {
+        console.log("You are not a registered voter.");
+    }
+}
+
+function countVotes() {
+    
+    for (let candidate in votes) {
+        console.log(`${candidate}: ${votes[candidate]} vote(s)`);
+    }
+    console.log();
+}
+
+
+
+
 function main(){
 	let hunterly = true
 	while(hunterly){
 		console.log("Voting System Menu");
 		console.log("1.Candidate Resgistration");
 		console.log("2.Voters Resgistration");
+		console.log("3.Voting");
+		console.log("4.count votes");
 		console.log("0.Exit");
 
 	let option = parseInt(prompt("enter an option from (1-6):"));
@@ -50,6 +85,14 @@ function main(){
 		case 2:
 			console.log("Voters Registration");
 			registerVoters();
+			break;
+		case 3:
+			console.log("Voting");
+			toVote();
+			break;
+		case 4:
+			console.log("Counting Votes");
+			countVotes();
 			break;
 		case 0:
 			console.log("bye bye ");
